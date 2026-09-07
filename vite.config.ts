eccf485f3e03fdd5d01f1c49698f661a91da2ff6
Sebastian Cortés <seba.cortesp@duocuc.cl>
2026-09-07 06:16:24 -0300

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES_BASE || '/',
+  base: process.env.GITHUB_PAGES_BASE || (process.env.NODE_ENV === 'production' ? '/Pong87/' : '/'),
   plugins: [vue()],
   server: {
     port: 5174,
